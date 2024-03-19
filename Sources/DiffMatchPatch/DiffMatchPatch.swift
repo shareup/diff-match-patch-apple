4fd93886aebea2a89678570aea9761a8f5881954
Anthony Drendel <me@anthonydrendel.com>
2024-03-19 23:18:31 +0100

    Add threeWayMerge()

--- Sources/DiffMatchPatch/DiffMatchPatch.swift
+++ Sources/DiffMatchPatch/DiffMatchPatch.swift
@@ -1,2 +1,31 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+import Foundation
+import diff_match_patch
+
+public func threeWayMerge(
+    old: String,
+    new: String,
+    base: String
+) throws -> String {
+    let dmp = DiffMatchPatch()
+    
+    guard let patch = dmp.patch_make(
+        fromOldString: old,
+        andNewString: new
+    ) as? [Any] else {
+        throw DiffMatchPatchError.couldNotCreatePatch
+    }
+
+    guard let result = dmp.patch_apply(
+        patch,
+        to: base
+    ).first as? String else {
+        throw DiffMatchPatchError.couldNotApplyPatch
+    }
+    
+    return result
+}
+
+public enum DiffMatchPatchError: Error {
+    case couldNotCreatePatch
+    case couldNotApplyPatch
+}
--- Tests/DiffMatchPatchTests/DiffMatchPatchTests.swift
+++ Tests/DiffMatchPatchTests/DiffMatchPatchTests.swift
@@ -2,11 +2,40 @@ import XCTest
 @testable import DiffMatchPatch
 
 final class DiffMatchPatchTests: XCTestCase {
-    func testExample() throws {
-        // XCTest Documentation
-        // https://developer.apple.com/documentation/xctest
-
-        // Defining Test Cases and Test Methods
-        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    func testThreeWayMerge() throws {
+        let old = """
+            Buy:
+            
+            * Milk
+            * Fresh bread
+            * Eggs (6)
+            """.trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        let new = """
+            To buy:
+            
+            * Milk
+            * Fresh bread (x2)
+            * Eggs (6)
+            """.trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        let base = """
+            Buy:
+            
+            * Milk
+            * Fresh bread
+            * Eggs (10)
+            """.trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        let expected = """
+            To buy:
+            
+            * Milk
+            * Fresh bread (x2)
+            * Eggs (10)
+            """.trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        let merged = try threeWayMerge(old: old, new: new, base: base)
+        XCTAssertEqual(merged, expected)
     }
 }
